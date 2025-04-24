b864d5dc3f15ce4200c62c243336db614fb1f0aa
dadulsawol <apriljoydeang23@gmail.com>
2025-04-25 00:40:07 +0800

    Added a dark and light mode feature

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ const navLinks = document.querySelector('.nav-links');
 menuIcon.onclick = () => {
     navLinks.classList.toggle('active');
 }
+
+function changeColorMode() {
+    toDarkOrLigt = document.querySelector('.dark-light-icon');
+
+    if (toDarkOrLigt.classList.contains('fa-moon')) {
+        document.querySelector('body').classList.add('to-dark');
+        document.querySelector('footer').classList.add('footer-to-dark');
+        document.querySelector('.dark-light-icon').classList.replace('fa-moon', 'fa-sun');
+    }
+
+    else {
+        document.querySelector('body').classList.remove('to-dark');
+        document.querySelector('footer').classList.remove('footer-to-dark');
+        document.querySelector('.dark-light-icon').classList.replace('fa-sun', 'fa-moon');
+    }
+}
